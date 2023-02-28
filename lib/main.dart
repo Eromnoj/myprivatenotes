6fffb47f4839497669203ed402fea2e48d8cf1ec
Jonathan Moreschi <j.moreschi@outlook.fr>
2023-02-28 15:11:00 +0100

    step-5

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myprivatenotes/constants/routes.dart';
 import 'package:myprivatenotes/firebase_options.dart';
 import 'package:myprivatenotes/views/login_view.dart';
 import 'package:myprivatenotes/views/register_view.dart';
@@ -19,9 +20,9 @@ void main() {
     // Map with name of the route as key, and a function that calls the view
     // as value
     routes: {
-      "/login/": (context) => const LoginView(),
-      "/register/": (context) => const RegisterView(),
-      "/notes/": (context) => const NotesView(),
+      loginRoute: (context) => const LoginView(),
+      registerRoute: (context) => const RegisterView(),
+      notesRoute: (context) => const NotesView(),
     },
   ));
 }
@@ -89,7 +90,7 @@ class _NotesViewState extends State<NotesView> {
 
                     if (context.mounted) {
                       Navigator.of(context).pushNamedAndRemoveUntil(
-                        '/login/',
+                        loginRoute,
                         (route) => false,
                       );
                     }
